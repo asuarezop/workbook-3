6908c757e7df9edd4fa15e07089ca5b15717f078
Anthony Suarez <asuarezop@gmail.com>
2024-10-09 13:33:02 -0400

    Fixed issue with output printing only last employee in PayrollCalApp.java

---
+++ PayrollCalculator/src/main/java/com/pluralsight/payroll/Employee.java
@@ -0,0 +1,45 @@
+package com.pluralsight.payroll;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee (int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    //Getters for Employee info
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public static double getGrossPay(double hoursWorked, double payRate) {
+        return hoursWorked * payRate;
+    }
+
+    public String toString(Employee e) {
+
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append("Employee ID: ").append(employeeId).append(" Employee Name: ")
+                .append(name).append(" Gross Pay: ").append(hoursWorked * payRate);
+        return sb.toString();
+    }
+}
